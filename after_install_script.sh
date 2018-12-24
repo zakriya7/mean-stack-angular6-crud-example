77d5625d7bfc29f36077d4147d4925c238192e13
zakriya7 <45287390+zakriya7@users.noreply.github.com>
2018-12-24 01:58:28 -0800

    Update after_install_script.sh

--- after_install_script.sh
+++ after_install_script.sh
@@ -1,5 +1,6 @@
-cd /usr/share/nginx/html/nodejs
- curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
- yum -y install nodejs > /dev/null 2> /dev/null < /dev/null &
- npm install -g @angular/cli > /dev/null 2> /dev/null < /dev/null &
- ng serve > /dev/null 2> /dev/null < /dev/null &
+#!bin/bash
+ cd /usr/share/nginx/html/nodejs
+  curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -
+  yum -y install nodejs > /dev/null 2> /dev/null < /dev/null &
+  npm install @angular/cli > /dev/null 2> /dev/null < /dev/null &
+  ng serve > /dev/null 2> /dev/null < /dev/null &
